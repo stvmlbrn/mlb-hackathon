06861d5f89c6344bd11494971cb77a2634a62dae
Steve Milburn <scmilburn@gmail.com>
2017-02-08 20:00:51 -0500

    added pitchSelectionTotals()

--- client/src/js/utils/banner.js
+++ client/src/js/utils/banner.js
@@ -5,7 +5,7 @@ module.exports = {
     var games = [];
 
     dataset.map(d => {
-      if (_.findIndex(games, function(g) {return g === d.gameDate}) === -1) {
+      if (_.findIndex(games, (g) => g === d.gameDate) === -1) {
         games.push(d.gameDate);
       }
     });
@@ -14,6 +14,29 @@ module.exports = {
   },
 
   avgPitchCount(appearances, pitches) {
-    return Math.round(pitches / appearances);
+    var avg = Math.round(pitches / appearances);
+    return !isNaN(avg) ? avg : 0;
+  },
+
+  pitchSelectionTotals(dataset) {
+    var totals = [];
+    var totalPitches = dataset.length;
+
+    dataset.map(d => {
+      var index = _.findIndex(totals, (t) => t.pitch === d.pitchType);
+
+      if (index === -1) {
+        totals.push({
+          pitch: d.pitchType,
+          count: 1,
+          percentage: 1 / totalPitches
+        });
+      } else {
+        totals[index].count++;
+        totals[index].percentage = ((totals[index].count / totalPitches) * 100).toFixed(2);
+      }
+    });
+
+    return totals;
   }
 };
