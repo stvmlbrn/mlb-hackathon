b930c54390c9d3e8906c06b44bf8a88039c6d794
Steve Milburn <scmilburn@gmail.com>
2017-02-11 18:38:22 -0500

    refactored appearances()

--- client/src/js/utils/banner.js
+++ client/src/js/utils/banner.js
@@ -4,15 +4,7 @@ import general from './general';
 
 module.exports = {
   appearances(dataset) {
-    var games = [];
-
-    dataset.map(d => {
-      if (_.findIndex(games, (g) => g === d.gameDate) === -1) {
-        games.push(d.gameDate);
-      }
-    });
-
-    return games.length;
+    return _.uniqBy(dataset, 'gameDate').length;
   },
 
   avgPitchCount(appearances, pitches) {
