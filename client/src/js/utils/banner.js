519bb7d839f792444f6dc4af8414310f9fa992a1
Steve Milburn <scmilburn@gmail.com>
2017-02-11 12:02:54 -0500

    removed pitchSelectionTotals()

--- client/src/js/utils/banner.js
+++ client/src/js/utils/banner.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 
+import general from './general';
+
 module.exports = {
   appearances(dataset) {
     var games = [];
@@ -16,27 +18,5 @@ module.exports = {
   avgPitchCount(appearances, pitches) {
     var avg = Math.round(pitches / appearances);
     return !isNaN(avg) ? avg : 0;
-  },
-
-  pitchSelectionTotals(dataset) {
-    var totals = [];
-    var totalPitches = dataset.length;
-
-    dataset.map(d => {
-      var index = _.findIndex(totals, (t) => t.pitch === d.pitchType);
-
-      if (index === -1) {
-        totals.push({
-          pitch: d.pitchType,
-          count: 1,
-          percentage: 1 / totalPitches
-        });
-      } else {
-        totals[index].count++;
-        totals[index].percentage = ((totals[index].count / totalPitches) * 100).toFixed(2);
-      }
-    });
-
-    return totals;
   }
 };
