f8751aab571d1c7c2aa2dc2733b20d83f2a6fb2e
Steve Milburn <scmilburn@gmail.com>
2017-02-11 20:40:56 -0500

    added colors to pie slices

--- client/src/js/components/charts/PieChart.jsx
+++ client/src/js/components/charts/PieChart.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import {PieChart, Pie, Legend, Tooltip, ResponsiveContainer} from 'recharts';
+import {PieChart, Pie, Legend, Sector, Cell, Tooltip, ResponsiveContainer} from 'recharts';
 
 import general from '../../utils/general';
 
-/* const data = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
-                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200},
-                  {name: 'Group E', value: 278}, {name: 'Group F', value: 189}]
-*/
+var colors = general.chartColors();
+
 function Chart(props) {
   var {data} = props;
+
+  data.map((d, index) => {
+    d.fill = colors[index];
+  });
 
   return (
     <ResponsiveContainer width="100%" height={400}>
       <PieChart>
-        <Pie data={data} margin={{top: 20, right: 30, left: 20, bottom: 10}} fill="#8884d8" label/>
+        <Pie data={data} margin={{top: 20, right: 30, left: 20, bottom: 10}} label/>
         <Tooltip/>
         <Legend/>
       </PieChart>
