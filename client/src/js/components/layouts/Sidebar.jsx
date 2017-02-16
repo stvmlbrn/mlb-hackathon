3a3a6e4df5d78e6ce67a4dc8850870ed1852aaff
Steve Milburn <scmilburn@gmail.com>
2017-02-16 09:24:23 -0500

    completed pitch reference and header text

--- client/src/js/components/layouts/Sidebar.jsx
+++ client/src/js/components/layouts/Sidebar.jsx
@@ -6,12 +6,33 @@ export default class extends Component {
     return (
       <div className="sidebar">
         <div className="brand">
-          <Link to="/">MLB Hackathon</Link>
+          <Link to="/"><h4 className="text-center">TruMedia<br/>MLB Hackathon</h4></Link>
         </div>
 
-        <div className="sidebar-scroll">
-          CH - Changeup<br/>
-          CU - Curveball<br/>
+        <div>
+          <h4 className="text-center">Pitch Reference</h4>
+          <ul>
+            <li>CH - Changeup</li>
+            <li>CU - Curveball</li>
+            <li>FA - Fastball</li>
+            <li>FT - Two Seamer</li>
+            <li>FF - Four Seamer</li>
+            <li>FC - Cutter</li>
+            <li>SL - Slider</li>
+            <li>FS - Splitter</li>
+            <li>SI - Sinker</li>
+            <li>FO - Forkball</li>
+            <li>KN - Knuckleball</li>
+            <li>KC - Knuckle Curve</li>
+            <li>SC - Screwball</li>
+            <li>GY - Gyroball</li>
+            <li>EP - Eephus</li>
+            <li>PO - Pitchout</li>
+            <li>IN - Intentional Ball</li>
+            <li>AB - Automatic Ball</li>
+            <li>AS - Automatic Strike</li>
+            <li>UN - Unknown</li>
+          </ul>
         </div>
       </div>
     );
