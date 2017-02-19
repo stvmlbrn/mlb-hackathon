5ed2d45b105d7a2c1829ebe2d6048709f7068151
Steve Milburn <scmilburn@gmail.com>
2017-02-19 18:26:17 -0500

    added responsive container

--- client/src/js/components/charts/BiaxialLineChart.jsx
+++ client/src/js/components/charts/BiaxialLineChart.jsx
@@ -7,7 +7,8 @@ function BiaxialLineChart(props) {
   var spinRateDomain = [spinRateMin, spinRateMax];
 
   return (
-      <LineChart width={800} height={300} data={data} margin={{top: 20, right: 30, left: 20, bottom: 5}}>
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart data={data} margin={{top: 20, right: 30, left: 20, bottom: 5}}>
         <XAxis dataKey="name"/>
         <YAxis yAxisId="left" orientation="left" type="number" domain={spinRateDomain} stroke="#8884d8"/>
         <YAxis yAxisId="right" orientation="right" type="number" domain={velocityDomain} stroke="#82ca9d"/>
@@ -17,6 +18,7 @@ function BiaxialLineChart(props) {
         <Line type="monotone" yAxisId="left" dataKey="avgSpinRate" stroke="#8884d8" />
         <Line type="monotone" yAxisId="right" dataKey="avgVelocity" stroke="#82ca9d" />
       </LineChart>
+    </ResponsiveContainer>
   );
 };
 
