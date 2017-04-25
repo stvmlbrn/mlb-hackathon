f22541d57655d203bebebf57a574ad6bbd934b28
Steve Milburn <scmilburn@gmail.com>
2017-04-25 18:53:37 -0400

    fixed linter issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-var path = require('path');
-var webpack = require('webpack');
-var WebpackNotifierPlugin = require('webpack-notifier');
+const path = require('path');
+const webpack = require('webpack');
+const WebpackNotifierPlugin = require('webpack-notifier');
 
-var PATHS = {
+const PATHS = {
   src: path.join(__dirname, 'client/src/js/'),
-  build: path.join(__dirname, 'client/public/build/')
+  build: path.join(__dirname, 'client/public/build/'),
 };
 
 module.exports = {
   entry: {
-    app: path.join(PATHS.src, 'app.jsx')
+    app: path.join(PATHS.src, 'app.jsx'),
   },
   output: {
     path: PATHS.build,
     filename: '[name].js',
-    publicPath: 'build/'
+    publicPath: 'build/',
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
   module: {
     loaders: [
@@ -26,47 +26,45 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'react', 'stage-1']
-        }
+          presets: ['es2015', 'react', 'stage-1'],
+        },
       },
       {
         test: /\.(jpg|png)$/,
-        loader: 'url-loader'
+        loader: 'url-loader',
       },
       {
         test: /\.scss$/,
         loaders: [
           'style-loader',
           'css-loader',
-          'sass-loader'
-        ]
+          'sass-loader',
+        ],
       },
       {
         test: /\.css/,
         loaders: [
           'style-loader',
-          'css-loader'
-        ]
+          'css-loader',
+        ],
       },
       {
         test: /\.(ttf|eot|svg|woff(2)?)(\S+)?$/,
-        loader: 'file-loader'
-      }
-    ]
+        loader: 'file-loader',
+      },
+    ],
   },
   plugins: [
     new webpack.ProvidePlugin({
-      'Promise': 'es6-promise'
+      Promise: 'es6-promise',
     }),
     new WebpackNotifierPlugin(),
     // Always expose NODE_ENV to webpack, in order to use `process.env.NODE_ENV`
-    // inside your code for any environment checks; UglifyJS will automatically
-    // drop any unreachable code.
+    // inside your code for any environment checks
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
       },
     }),
-    //new webpack.optimize.UglifyJsPlugin()
-  ]
+  ],
 };
