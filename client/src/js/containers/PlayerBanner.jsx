2076ea34434fd8d8b3454c88320c302a7d809788
Steve Milburn <scmilburn@gmail.com>
2017-04-25 18:54:45 -0400

    linter issues and prop types validation

--- client/src/js/containers/PlayerBanner.jsx
+++ client/src/js/containers/PlayerBanner.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import PlayerPhoto from '../components/playerPhoto';
 import PanelNoControls from '../components/panels/PanelNoControls';
 import Box from '../components/box';
 
 function PlayerBanner(props) {
-  var {data, pitcherId, name, selectSeason, season} = props;
+  const { data, pitcherId, name, selectSeason, season } = props;
+
   return (
     <PanelNoControls>
       <div className="row">
         <div className="col-md-3">
           <div className="media">
             <div className="media-left">
-              <PlayerPhoto playerId={pitcherId} height="125"/>
+              <PlayerPhoto playerId={pitcherId} height="125" />
             </div>
             <div className="media-body">
               <h3>{name}</h3>
-              <select name="season" className="form-control" onChange={selectSeason} value={season} >
+              <select name="season" className="form-control" onChange={selectSeason} value={season}>
                 <option value="2016">Season: 2016</option>
                 <option value="2015">Season: 2015</option>
                 <option value="2014">Season: 2014</option>
@@ -25,17 +27,25 @@ function PlayerBanner(props) {
           </div>
         </div>
         <div className="col-md-3">
-          <Box number={data.appearances} title="Appearances" icon="fa fa-video-camera"/>
+          <Box number={data.appearances} title="Appearances" icon="fa fa-video-camera" />
         </div>
         <div className="col-md-3">
-          <Box number={data.totalPitches} title="Total Pitches" icon="fa fa-line-chart"/>
+          <Box number={data.totalPitches} title="Total Pitches" icon="fa fa-line-chart" />
         </div>
         <div className="col-md-3">
-          <Box number={data.avgPitchCount} title="Avg Pitch Count" icon="fa fa-bars"/>
+          <Box number={data.avgPitchCount} title="Avg Pitch Count" icon="fa fa-bars" />
         </div>
       </div>
     </PanelNoControls>
   );
 }
+
+PlayerBanner.propTypes = {
+  data: PropTypes.object.isRequired,
+  pitcherId: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  selectSeason: PropTypes.func.isRequired,
+  season: PropTypes.number.isRequired,
+};
 
 export default PlayerBanner;
